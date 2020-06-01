17fe1fc1ae505bc5b9c31bdafe0a5b05f13363aa
vivekchauhan12000 <49834398+vivekchauhan12000@users.noreply.github.com>
2020-06-01 12:14:44 +0530

    commit_3

--- App.js
+++ App.js
@@ -1,114 +1,198 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Mainstore from "./Mainstore";
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import Mywallet from "./components/Mywallet";
+import Mycart from "./components/Mycart";
+import Mywishlist from "./components/Mywishlist";
+import Myorder from "./components/Myorder";
+import Myaccount from "./components/Myaccount";
+import Appliance from "./pages/Appliance";
+import Herbal from "./pages/Herbal";
+import Combo from "./pages/Combo";
+import Kid from "./pages/Kid";
+import Women from "./pages/Women";
+import HomeScreen from "./pages/HomeScreen";
+import SettingsScreen from "./pages/SettingScreen"
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
+
+
+
+
+
+function Menwear({ navigation }) {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <View style={{ flex: 1}}>
+     
+      <Text>Men wear</Text>
+    
+    </View>
   );
-};
+}
+function Homeandkit({ navigation }) {
+  return (
+    <View style={{ flex: 1}}>
+     
+      <Text>Home and kitchen</Text>
+    
+    </View>
+  );
+}
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+function More({ navigation }) {
+  return (
+    <View style={{ flex: 1}}>
+     
+      <Text>More</Text>
+    
+    </View>
+  );
+}
 
-export default App;
+const Tab = createMaterialTopTabNavigator();
+const Drawer = createDrawerNavigator();
+export default function App() {
+  return (
+    
+    <View style={{flex:1,flexDirection:"column",justifyContent:"flex-start"}}>
+     <Mainstore/>
+     <NavigationContainer style={{flex:1}}>
+   
+     <Drawer.Navigator initialRouteName="Home"
+      drawerStyle={{
+        backgroundColor: 'white',
+        width: 240,
+      }}
+      drawerContentOptions={{
+        activeTintColor: 'red',
+       
+      }}
+      >
+      <Drawer.Screen name="Home" component={Prevnav} 
+      options={{ 
+      drawerIcon:({color})=><Icon name="tags" size={20} color={color}/> }}/>
+
+      <Drawer.Screen name="Electronics" component={SettingsScreen}
+      options={{ 
+      drawerIcon:({color})=><Icon name="tv" size={20} color={color}/> }} />
+
+      <Drawer.Screen name="Men wear" component={Menwear}
+      options={{ 
+      drawerIcon:({color})=><Icon name="user-secret" size={20} color={color}/> }} />
+
+      <Drawer.Screen name="Home and kitchen" component={Homeandkit}
+      options={{ 
+      drawerIcon:({color})=><Icon name="glass" size={20} color={color}/> }} />
+
+      <Drawer.Screen name="More" component={More}
+      options={{ 
+      drawerIcon:({color})=><Icon name="th" size={20} color={color}/> }}
+       />
+
+      <Drawer.Screen name="Appliance" component={Appliance}
+      options={{ 
+      drawerIcon:({color})=><Icon name="plug" size={20} color={color}/> }} />
+      
+      <Drawer.Screen name="Herbal" component={Herbal}
+      options={{ 
+      drawerIcon:({color})=><Icon name="coffee" size={20} color={color}/> }} />
+       
+       <Drawer.Screen name="Combo" component={Combo}
+      options={{ 
+      drawerIcon:({color})=><Icon name="th-large" size={20} color={color}/> }} />
+
+        <Drawer.Screen name="Kid" component={Kid}
+      options={{ 
+      drawerIcon:({color})=><Icon name="street-view" size={20} color={color}/> }} />
+       
+       <Drawer.Screen name="Women" component={Women}
+      options={{ 
+      drawerIcon:({color})=><Icon name="diamond" size={20} color={color}/> }} />
+
+    
+      <Drawer.Screen name="My wallet" component={Mywallet}
+      options={{ 
+      drawerIcon:({color})=><Icon name="dollar" size={20} color={color}/> }} />
+
+      <Drawer.Screen name="My cart" component={Mycart}
+      options={{ 
+      drawerIcon:({color})=><Icon name="shopping-cart" size={20} color={color}/> }} />
+
+      <Drawer.Screen name="Wishlist" component={Mywishlist}
+      options={{ 
+      drawerIcon:({color})=><Icon name="list-alt" size={20} color={color}/> }} />
+
+      <Drawer.Screen name="My order" component={Myorder}
+      options={{ 
+      drawerIcon:({color})=><Icon name="briefcase" size={20} color={color}/> }} />
+
+
+      <Drawer.Screen name="My account" component={Myaccount}
+      options={{ 
+      drawerIcon:({color})=><Icon name="user-o" size={20} color={color}/> }} />
+
+
+      </Drawer.Navigator>
+    
+  </NavigationContainer >
+  </View>
+  );
+}
+    Prevnav=()=> <Tab.Navigator
+      initialRouteName="Feed"
+      tabBarOptions={{
+      activeTintColor: 'white',
+      inactiveTintColor:"white",
+      labelStyle: { fontSize: 10 },
+      showIcon:true,
+     style: { backgroundColor: 'red' },
+    }}
+>
+<Tab.Screen
+  name="Home"
+  component={HomeScreen}
+  options={{ tabBarLabel: 'Home',
+tabBarIcon:({color})=><Icon name="tags" size={20} color={color}/> }}
+  
+  
+/> 
+
+<Tab.Screen
+  name="Electronics"
+  component={SettingsScreen}
+  options={{ tabBarLabel: 'Electronics',
+  tabBarIcon:({color})=><Icon name="tv" size={20} color={color}/> }}
+  
+/>
+
+ <Tab.Screen
+  name="Men wear"
+  component={Menwear}
+  options={{ tabBarLabel: 'Mean wear',
+  tabBarIcon:({color})=><Icon name="user-secret" size={20} color={color}/> }}
+  /> 
+
+<Tab.Screen
+  name="Homeandkit"
+  component={Homeandkit}
+  options={{ tabBarLabel: 'Home & kitchen',
+  tabBarIcon:({color})=><Icon name="glass" size={20} color={color}/> }}
+  /> 
+
+
+<Tab.Screen
+  name="More"
+  component={More}
+  options={{ tabBarLabel: 'More',
+  tabBarIcon:({color})=><Icon name="th" size={20} color={color}/> }}
+  /> 
+  
+ 
+
+</Tab.Navigator>
